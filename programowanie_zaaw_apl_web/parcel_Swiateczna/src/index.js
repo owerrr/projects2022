4b742637ff3adef9359de332175848fea8ba89b5
Prorog <proroog@gmail.com>
2023-03-06 22:45:41 +0100

    06.03.2023 - Testowanie i dokumentowanie aplikacji
    [/] kalkulator

---
+++ programowanie_zaaw_apl_web/parcel_Swiateczna/src/index.js
@@ -0,0 +1,102 @@
+"use strict";
+const pattern = [
+    "x", "x", "x",
+    "x", "x", "x",
+    "x", "x", "x",
+    "x", "x", "x",
+    "x", "x", "x",
+    "x", "x", "x",
+    "x", "x", "x",
+    "x", "x", "x",
+];
+const app = document.querySelector("#app");
+const dateNow = new Date();
+const adventDays = 24;
+const cookieName = "generatedJokes";
+var JokeList = [];
+0;
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+window.onload = async () => {
+    generateJokes();
+    // document.querySelector("#app")!.innerHTML = post.value;
+};
+async function generateJokes() {
+    if (getCookie(cookieName) == "") {
+        var posts = [];
+        for (var i = 0; i < adventDays; i++) {
+            const req = await fetch("https://api.chucknorris.io/jokes/random");
+            var post = await req.json();
+            posts.push(post.value);
+        }
+        posts.forEach((el) => {
+            console.log(el);
+        });
+        document.cookie = "generatedJokes=true;";
+        // console.log(JSON.stringify(posts));  
+        document.cookie = "Jokes=" + JSON.stringify(posts) + ";";
+        document.cookie = "Opened=";
+    }
+    var JokesCookie = getCookie("Jokes");
+    var temp = JokesCookie.split('","');
+    temp.forEach((el) => {
+        el = el.replace(/[^\w\s]/gi, '');
+        JokeList.push(el);
+    });
+    generatePattern();
+}
+function generatePattern() {
+    for (var i = 0; i < pattern.length; i++) {
+        var temp = document.createElement("div");
+        temp.id = "" + (i + 1);
+        temp.innerHTML = "<span>" + (i + 1) + "</span>";
+        temp.innerHTML += `<p>${i + 1 + ". " + JokeList[i]}</p>`;
+        var openedCookies = getCookie("Opened");
+        if (i < dateNow.getDate())
+            temp.className = "adventGift adventGiftAvailable";
+        else
+            temp.className = "adventGift adventGiftUnavailable";
+        openedCookies.split(',').forEach((el) => {
+            if (el == "")
+                return;
+            if (parseInt(el) == i + 1) {
+                (app.children[0]).innerHTML += `<p>${el}</p>`;
+                temp.innerHTML = "✔";
+                temp.setAttribute("class", "adventGift");
+            }
+        });
+        (app.children[1]).appendChild(temp);
+    }
+    document.querySelectorAll(".adventGiftAvailable").forEach((el) => {
+        el.addEventListener("click", (event) => {
+            console.log(event.target);
+            var div = event.target;
+            var openedCookie = getCookie("Opened");
+            div.setAttribute("class", "adventGift");
+            var Joke = div.children[1].innerHTML;
+            openedCookie += Joke;
+            app.children[0].innerHTML += `<p>${Joke}</p>`;
+            div.innerHTML = "✔";
+            document.cookie = "Opened=" + openedCookie + ",";
+        });
+    });
+    document.querySelectorAll(".adventGiftUnavailable").forEach((el) => {
+        el.addEventListener("click", (event) => {
+            alert("Jeszcze nie możesz otworzyć tego!");
+        });
+    });
+}
+console.log("Generated Jokes: " + getCookie("generatedJokes"));
--- testowanie_dokumentowanie_aplikacji/Kalkulator/README.md
+++ testowanie_dokumentowanie_aplikacji/Kalkulator/README.md
@@ -1,5 +1,43 @@
 # Kalkualtor w HTML
-Został on opisany w kodzie poszczególnych plików.
+Program wykonujący obliczenia na liczbach całkowitych i zmiennoprzecinkowych.
+
+<br/>
+
+### Słownik
+- *LoadButtons()* - załadowanie przycisków w kalkulatorze
+- *UseButton()* - funkcja odczytująca wciśniety przycisk
+- *useSymbol()* - funkcja zapisująca do operacji dany symbol
+- *useNumber()* - funkcja zapisująca do operacji daną liczbe
+- *calculate()* - funkcja sprawdzająca liczbę widoczną w polu wyniku
+- *operation()* - funkcja wykonująca operacje na liczbach
+<br/>
+
+### Opis
+- Aplikacja działa w języku JS z pomocą HTML oraz CSS
+- System odczytuje przyciski które wciska użytkownik, a następnie wykonuje operacje
+- Aplikacja może być używana przez każdego użytkownika
+- Jest ona kompatybilna z każdym systemem operacyjnym, który ma możliwość skorzystania z przeglądarki internetowej obsługującej javascript
+
+<br/>
+
+### Wymagania funkcjonalne
+- Funkcjonalność - operation()
+- Opis - jest wykorzystywania do operacji na liczbach
+- Wejscie - Wcisniete przyciski
+- Wyjscie - Wynik operacji na liczbach
+- Efekty uboczne - można łatwo zmanipulować skrypt poprzez wejśćie w zbadaj element, zmanipulowany skrypt wykonuje się tylko u tej samej osoby do momentu odświeżenia strony
+
+<br/>
+
+### Wymagania niefunkcjonalne
+- Każdy możliwy system, który posiada oraz jest w stanie uruchomić przeglądarke internetową obsługująca javascript
+- Aplikacja jest w stanie działać na słabym sprzęcie
+- Brak ograniczeń prawnych
+- Program nie ma żadnych problemów z wydajnością, gdy jest on poprawnie użytkowany
+
+<br/>
+
+### Pliki źródłowe
 
 wykorzystano: `HTML5, CSS3, JavaScript`<br/>
 główny plik: `./views/index.html`<br/>
