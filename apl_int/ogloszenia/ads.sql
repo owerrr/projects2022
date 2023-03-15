0bc23c4f89dc8f111d17b4250261b20393207aa0
Prorog <proroog@gmail.com>
2023-03-15 21:19:09 +0100

    Update ads.sql

--- apl_int/ogloszenia/ads.sql
+++ apl_int/ogloszenia/ads.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 22 Sty 2023, 17:10
--- Wersja serwera: 10.4.24-MariaDB
--- Wersja PHP: 8.1.6
+-- Czas generowania: 15 Mar 2023, 21:18
+-- Wersja serwera: 10.4.14-MariaDB
+-- Wersja PHP: 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,35 @@ SET time_zone = "+00:00";
 --
 -- Baza danych: `ads`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `ads`
+--
+
+CREATE TABLE `ads` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `content` mediumtext DEFAULT NULL,
+  `create_date` datetime DEFAULT current_timestamp(),
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Zrzut danych tabeli `ads`
+--
+
+INSERT INTO `ads` (`id`, `title`, `content`, `create_date`, `user_id`) VALUES
+(11, 'dfhfdhggg fg h', 'jakas tam wiadomosc', '2023-03-05 21:58:05', 1),
+(13, 'jaksi tam postdgdfgdf dfhdfhdfhf ghjs', 'jakas tam wiadomosc', '2023-03-05 21:58:05', 1),
+(14, 'jaksi ta postdgdfgdf dfhdfhdfhfcdfvb', 'jakas tam wiadomosc', '2023-03-05 21:58:05', 2),
+(15, 'jaksi tam postdgdfgdf dfhdfhdfhf', 'jakas tam wiadomosc', '2023-03-05 21:58:05', 2),
+(16, 'jaksi tam postdgdfgdf dfhdfhdfhf', 'jakas tam wiadomosc', '2023-03-05 21:58:05', 1),
+(17, 'aha test', 'jakas tam wiadomosc', '2023-03-05 22:03:33', 1),
+(18, 'jakas nazwa', 'opis testwoy dodawanie', '2023-03-06 15:35:36', 2),
+(19, 'fghfghfgh', 'fhfghf', '2023-03-06 15:39:06', 2),
+(20, 'ghjgh', 'jghjghj', '2023-03-06 15:39:11', 1);
 
 -- --------------------------------------------------------
 
@@ -39,11 +68,18 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
-(1, 'Admin', 'admin@gmail.com', '$2y$10$reOd26W1DnqdWNwILizi/exuLeWP22V76HBLSHdhjYi4HKcwVIuTm');
+(1, 'Jasonj', 'admin@gmail.com', '$2y$10$reOd26W1DnqdWNwILizi/exuLeWP22V76HBLSHdhjYi4HKcwVIuTm'),
+(2, 'user', 'user@gmail.com', '$2y$10$reOd26W1DnqdWNwILizi/exuLeWP22V76HBLSHdhjYi4HKcwVIuTm');
 
 --
 -- Indeksy dla zrzutów tabel
 --
+
+--
+-- Indeksy dla tabeli `ads`
+--
+ALTER TABLE `ads`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeksy dla tabeli `users`
@@ -52,14 +88,20 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT dla zrzuconych tabel
+-- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT dla tabeli `ads`
+--
+ALTER TABLE `ads`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
